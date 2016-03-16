68a080da0d569a66fdc4f8991c11a259f9e4e836
Ricardo Sánchez-Sáez <rsanchez.saez@gmail.com>
2016-03-16 17:09:07 +0000

    [CBDSceneKit] Minor improvements to the comments

--- iOS Examples/CBDSceneKit/Source/SceneKitViewController.m
+++ iOS Examples/CBDSceneKit/Source/SceneKitViewController.m
@@ -45,12 +45,13 @@
     
     _scene = [SCNScene sceneNamed:@"scene.scnassets/scene.dae"];
     
-    // cameraControlNode is the parent of the Camera node, we can use this to position/rotate the camera without affecting the head tracking
+    // _cameraControlNode is the parent of _cameraNode
+    // it can be used to translate/rotate the camera without affecting the head tracking
     _cameraControlNode = [_scene.rootNode childNodeWithName:@"CameraControl" recursively:YES];
-    // cameraNode is the node that actually controls the head tracking applied to the camera
+    // _cameraNode is where the head tracking transform is applied
     _cameraNode = [_scene.rootNode childNodeWithName:@"Camera" recursively:YES];
     
-    // SpriteKitCube is a cube in the scene which we will apply the SKScene as a texture (nothing in the scene but that's irrelevant)
+    // spriteKitCube is a cube which has a SKScene applied as a texture (the texture is empty but that's irrelevant)
     SCNNode *spriteKitCube = [_scene.rootNode childNodeWithName:@"SpriteKitCube" recursively:YES];
     spriteKitCube.geometry.firstMaterial.diffuse.contents = [[SKScene alloc] initWithSize:CGSizeMake(100.0f, 100.0f)];
     
@@ -58,9 +59,9 @@
     _renderer.scene = _scene;
     _renderer.pointOfView = _cameraNode;
     
-    // Example of moving the CameraControlNode while not affecting the head tracking
-    // The position and rotation of this node is where the camera would be looking if it was straight ahead
-    // The head tracking rotation appleid to the cameraNode is applied after this so you get both effects
+    // Example of moving _cameraControlNode without affecting the head tracking
+    // The position and rotation of _cameraControlNode determines where the camera is looking when the user head is looking forward
+    // The head tracking transform is applied to _cameraNode independently of _cameraControlNode, so you get the combined transform
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         SCNAction *cameraMoveAction = [SCNAction moveTo:SCNVector3Make(-4.5f, -4.5f, 0.0f) duration:10.0f];
         cameraMoveAction.timingMode = SCNActionTimingModeEaseInEaseOut;
@@ -78,19 +79,17 @@
 
 - (void)prepareNewFrameWithHeadViewMatrix:(GLKMatrix4)headViewMatrix
 {
-    // NOTE: Disabling the Scissor Test here due to the issue Scissor Test is causing on some devices
-    // Parts of the screen are left uncleared without this.
-    // It is enabled again after returning from this function so no need to re-enable here.
+    // Disable GL_SCISSOR_TEST here due to an issue that causes parts of the screen not to be cleared on some devices
+    // GL_SCISSOR_TEST is enabled again after returning from this function so no need to re-enable here.
     glDisable(GL_SCISSOR_TEST);
-    // NOTE: We do the glClear here because of SpriteKit being used as a texture
-    // If you move this to the start of drawEyeWithEye you will see the broken scissor
-    // that will clear the left side of the screen when the right eye is drawn
+    // Perform glClear() because using SpriteKit's SKScene as a texture in SceneKit interferes with GL_SCISSOR_TEST
+    // If you move glClear() to the start of -drawEyeWithEye:, the left side of the screen is cleared when the right eye is drawn
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 - (void)drawEyeWithEye:(CBDEye *)eye
 {
-    // Because we are using a Blender Exported Scene, we have to use Z-Up (and hence Y-Forward)
+    // Use Z-Up/Y-Forward because we are using a scene exported from Blender
     GLKMatrix4 lookAt = GLKMatrix4MakeLookAt(0.0f, 0.0f, 0.0f,
                                              0.0f, 1.0f, 0.0f,
                                              0.0f, 0.0f, 1.0f);    
